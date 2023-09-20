b81ecfa79e254f366acea72681f6790953c34fad
Cássia <cascrisbern@gmail.com>
2023-09-20 12:13:36 +0300

    fix: it was taken out a middleware that was going to all the routes

--- src/middlewares/verifyUserAuthorization.js
+++ src/middlewares/verifyUserAuthorization.js
@@ -1,8 +1,13 @@
+const AppError = require("../utils/AppError")
+
 function verifyUserAuthorization(roleToVerify) {
 	return (req, res, next) => {
 		const { role } = req.user
 
+		console.log(role)
+
 		if (!roleToVerify.includes(role)) {
+			console.log("Entrei aqui")
 			throw new AppError("Unauthorized", 401)
 		}
 
