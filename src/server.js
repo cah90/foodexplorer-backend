2f627945f8177cff22979df6135afa2481a5118f
Cássia <cascrisbern@gmail.com>
2023-09-25 20:56:40 +0300

    preparing for deploy

--- src/server.js
+++ src/server.js
@@ -17,7 +17,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+		origin: [
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"https://rocketseat-foodexplorer.netlify.app",
+		],
 		credentials: true,
 	})
 )
